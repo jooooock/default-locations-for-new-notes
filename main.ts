2f95e10e8a4f6bc2956b0be2b371b105524916af
jooooock <findsource@proton.me>
2024-02-20 20:10:14 +0800

    init

--- main.ts
+++ main.ts
@@ -1,134 +1,155 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+    App,
+    Editor,
+    MarkdownView,
+    Modal,
+    Notice,
+    Plugin,
+    PluginManifest,
+    PluginSettingTab,
+    Setting,
+    TFolder
+} from 'obsidian';
+import * as path from "path";
 
-// Remember to rename these classes and interfaces!
 
 interface MyPluginSettings {
-	mySetting: string;
+    type: 'vault' | 'current' | 'folder'
+    custom: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+    type: 'vault',
+    custom: '',
 }
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+    settings: MyPluginSettings;
+    mdFileCreator: (docPath: string) => TFolder;
 
-	async onload() {
-		await this.loadSettings();
+    constructor(app: App, manifest: PluginManifest) {
+        super(app, manifest)
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+        this.mdFileCreator = this.getMarkdownNewFileParent.bind(this)
+    }
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+    getMarkdownNewFileParent(docPath: string) {
+        if (this.settings.type === 'vault') {
+            return this.app.vault.getRoot()
+        } else if (this.settings.type === 'current') {
+            const currentDir = path.dirname(docPath)
+            const target = this.app.vault.getAbstractFileByPath(currentDir)
+            if (target instanceof TFolder) {
+                return target
+            }
+        } else if (this.settings.type === 'folder') {
+            let targetFolder = ''
+            if (this.settings.custom.startsWith('${current}')) {
+                targetFolder = this.settings.custom.replace('${current}', path.dirname(docPath))
+            } else if (this.settings.custom.startsWith('${root}')) {
+                targetFolder = this.settings.custom.replace('${root}', '')
+            } else {
+                targetFolder = this.settings.custom
+            }
+            targetFolder = targetFolder.replace(/^\//, '')
+            if (targetFolder === '') {
+                targetFolder = '/'
+            }
+            console.log(targetFolder)
+            const target = this.app.vault.getAbstractFileByPath(targetFolder)
+            console.log(target)
+            if (target instanceof TFolder) {
+                return target
+            }
+        }
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+        new Notice('插件目录配置错误，新笔记将存放在仓库的根目录')
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+        return this.app.vault.getRoot()
+    }
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+    async onload() {
+        await this.loadSettings();
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+        // This adds a settings tab so the user can configure various aspects of the plugin
+        this.addSettingTab(new SampleSettingTab(this.app, this));
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
+        this.app.fileManager.registerFileParentCreator('md', this.mdFileCreator)
+    }
 
-	onunload() {
+    onunload() {
+        this.app.fileManager.unregisterFileCreator('md')
+    }
 
-	}
+    async loadSettings() {
+        this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+    }
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
+    async saveSettings() {
+        await this.saveData(this.settings);
+    }
 }
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+    plugin: MyPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+    constructor(app: App, plugin: MyPlugin) {
+        super(app, plugin);
+        this.plugin = plugin;
+    }
 
-	display(): void {
-		const {containerEl} = this;
+    display(): void {
+        const {containerEl} = this;
 
-		containerEl.empty();
+        containerEl.empty();
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+        let customFolderSetting: Setting | null = null
+        new Setting(containerEl)
+            .setName('Default location for new notes')
+            .setDesc('Where newly created notes are placed.')
+            .addDropdown(dropdown => {
+                dropdown
+                    .addOptions({
+                        'vault': 'Vault Folder',
+                        'current': 'Same folder as current file',
+                        'folder': 'In the folder specified below'
+                    })
+                    .onChange(async (value) => {
+                        this.plugin.settings.type = value as any
+                        await this.plugin.saveSettings()
+
+                        if (value === 'folder') {
+                            if (!customFolderSetting) {
+                                customFolderSetting = new Setting(containerEl)
+                                    .setName('Folder to create new notes in')
+                                    .setDesc('Newly created notes will appear under this folder.')
+                                    .addText(text => text
+                                        .setPlaceholder('Enter your secret')
+                                        .setValue(this.plugin.settings.custom)
+                                        .onChange(async (value) => {
+                                            this.plugin.settings.custom = value.replace(/^\/+/, '');
+                                            await this.plugin.saveSettings();
+                                        }))
+                            }
+                        } else {
+                            customFolderSetting?.settingEl.remove()
+                            customFolderSetting = null
+                        }
+                    })
+                    .setValue(this.plugin.settings.type)
+
+                if (this.plugin.settings.type === 'folder' && !customFolderSetting) {
+                    customFolderSetting = new Setting(containerEl)
+                        .setName('Folder to create new notes in')
+                        .setDesc('Newly created notes will appear under this folder.')
+                        .addText(text => text
+                            .setPlaceholder('Enter your secret')
+                            .setValue(this.plugin.settings.custom)
+                            .onChange(async (value) => {
+                                this.plugin.settings.custom = value.replace(/^\/+/, '');
+                                await this.plugin.saveSettings();
+                            }))
+                }
+            })
+    }
 }
